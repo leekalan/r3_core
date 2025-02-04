c1a7806165dcf354f044fc84850de497bc24e334
leekalan <kalanjlee@gmail.com>
2025-02-05 00:53:12 +1100

    make test coloured

--- src/main.rs
+++ src/main.rs
@@ -41,23 +41,23 @@ impl Vertex {
 const VERTICES: &[Vertex] = &[
     Vertex {
         position: [-0.0868241, 0.49240386, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [1.0, 0.0, 0.0],
     }, // A
     Vertex {
         position: [-0.49513406, 0.06958647, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [1.0, 1.0, 0.0],
     }, // B
     Vertex {
         position: [-0.21918549, -0.44939706, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [0.0, 1.0, 0.0],
     }, // C
     Vertex {
         position: [0.35966998, -0.3473291, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [0.0, 0.0, 1.0],
     }, // D
     Vertex {
         position: [0.44147372, 0.2347359, 0.0],
-        color: [0.5, 0.0, 0.5],
+        color: [1.0, 0.0, 1.0],
     }, // E
 ];
 
